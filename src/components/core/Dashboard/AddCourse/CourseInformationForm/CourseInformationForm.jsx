580b3f31cb388294a1150b1e5565f9fbd8b594d5
mantraAtAeonx <mantra.gor@aeonx.digital>
2024-12-14 19:55:33 +0530

    some fixes in course creation for s3 upload

--- src/components/core/Dashboard/AddCourse/CourseInformationForm/CourseInformationForm.jsx
+++ src/components/core/Dashboard/AddCourse/CourseInformationForm/CourseInformationForm.jsx
@@ -15,6 +15,7 @@ import {
 } from "../../../../../services/operations/courseDetailsApi";
 import KeyFeatures from "./KeyFeatures";
 import { uploadToS3 } from "../../../../../services/aws/s3Services";
+import Spinner from "../../../../ui/spinner/Spinner";
 
 function CourseInformationForm() {
   const {
@@ -30,6 +31,8 @@ function CourseInformationForm() {
   const [loading, setLoading] = useState(false);
   const [thumbnailMeta, setThumbnailMeta] = useState(null);
   const [courseCategories, setCourseCategories] = useState([]);
+
+  toast.error("ERROR");
 
   // Fetch categories and set initial form values if editing
   useEffect(() => {
@@ -78,195 +81,208 @@ function CourseInformationForm() {
 
   // Handle form submission
   const onSubmit = async (data) => {
-    if (!thumbnailMeta) {
-      return toast.error("Please upload a thumbnail");
-    }
-
-    // upload file to s3
-    const { uploadResponse, fileKey } = await uploadToS3(
-      thumbnailMeta,
-      data.file
-    );
-    if (uploadResponse.status !== 200) {
-      return toast.error("Failed to upload file!");
-    }
-
-    const fileData = new FormData();
-
-    let payload = {};
-    if (editCourse) {
-      if (!isFormUpdated()) return toast.error("No changes detected.");
-      // Add only changed fields to FormData
-    } else {
-      // eslint-disable-next-line no-unused-vars
-      const { file, ...restOfData } = data;
-      payload = {
-        ...restOfData,
-        fileKey,
-        status: COURSES_STATUSES.DRAFT,
-      };
-    }
-
     setLoading(true);
-    const result = editCourse
-      ? await updateCourse(payload, fileData)
-      : await addCourse(payload, payload);
-    setLoading(false);
+    try {
+      if (!thumbnailMeta) {
+        return toast.error("Please upload a thumbnail");
+      }
 
-    if (result?.success) {
-      dispatch(setStep(2));
-      dispatch(setCourse(result));
+      // upload file to s3
+      const { uploadResponse, fileKey } = await uploadToS3(
+        thumbnailMeta,
+        data.file,
+        "thumbnail"
+      );
+      if (uploadResponse.status !== 200) {
+        return toast.error("Failed to upload file!");
+      }
+
+      const fileData = new FormData();
+
+      let payload = {};
+      if (editCourse) {
+        if (!isFormUpdated()) return toast.error("No changes detected.");
+        // Add only changed fields to FormData
+      } else {
+        // eslint-disable-next-line no-unused-vars
+        const { file, ...restOfData } = data;
+        payload = {
+          ...restOfData,
+          fileKey,
+          status: COURSES_STATUSES.DRAFT,
+        };
+      }
+
+      const result = editCourse
+        ? await updateCourse(payload, fileData)
+        : await addCourse(payload, payload);
+      setLoading(false);
+
+      if (result?.success) {
+        dispatch(setStep(2));
+        dispatch(setCourse(result));
+      }
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
     }
   };
 
   return (
     <div>
-      <form
-        onSubmit={handleSubmit(onSubmit)}
-        className="rounded-md border-richblack-700 bg-richblack-800 p-6 space-y-5 mt-4"
-      >
-        <h2 className="text-xl font-semibold">Course Information</h2>
-        <div className="grid gap-y-1">
-          <label htmlFor="title">
-            Course Title <sup className=" text-[0.725rem] text-pink-200">*</sup>
-          </label>
-          <input
-            type="text"
-            id="title"
-            name="title"
-            placeholder="Enter Course Title"
-            className={`w-full bg-richblack-700 rounded-[0.5rem] text-richblack-5 p-[12px] shadow-richblack`}
-            style={{
-              boxShadow: " 0px -1px 0px 0px rgba(255, 255, 255, 0.18) inset",
-            }}
-            {...register("title", { required: true })}
-          />
-          {errors.title && (
-            <span className="text-pink-200 text-sm">
-              Course Title is required
-            </span>
-          )}
+      {loading ? (
+        <div className="w-full min-h-[400px] flex items-center">
+          <Spinner />
         </div>
-        <div className="grid gap-y-1">
-          <label htmlFor="description">
-            Course Short Description{" "}
-            <sup className=" text-[0.725rem] text-pink-200">*</sup>
-          </label>
-          <textarea
-            type="text"
-            id="description"
-            name="description"
-            placeholder="Enter Course Short Description"
-            rows={3}
-            className={`w-full bg-richblack-700 rounded-[0.5rem] text-richblack-5 p-[12px] shadow-richblack`}
-            style={{
-              boxShadow: " 0px -1px 0px 0px rgba(255, 255, 255, 0.18) inset",
-            }}
-            {...register("description", { required: true })}
-          ></textarea>
-          {errors.description && (
-            <span className="text-pink-200 text-sm">
-              Course Short Description is required
-            </span>
-          )}
-        </div>
-        <div className="grid gap-y-1">
-          <label htmlFor="price">
-            Course Price <sup className=" text-[0.725rem] text-pink-200">*</sup>
-          </label>
-          <div
-            style={{
-              boxShadow: " 0px -1px 0px 0px rgba(255, 255, 255, 0.18) inset",
-            }}
-            className="w-full flex items-center relative bg-richblack-700 rounded-[0.5rem] text-richblack-5 shadow-richblack"
-          >
-            <HiOutlineCurrencyRupee
-              className=" bottom-4 ml-3 opacity-70"
-              size={23}
-            />
+      ) : (
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="rounded-md border-richblack-700 bg-richblack-800 p-6 space-y-5 mt-4"
+        >
+          <h2 className="text-xl font-semibold">Course Information</h2>
+          <div className="grid gap-y-1">
+            <label htmlFor="title">
+              Course Title{" "}
+              <sup className=" text-[0.725rem] text-pink-200">*</sup>
+            </label>
             <input
               type="text"
-              id="price"
-              name="price"
-              placeholder="Enter Course Price"
-              className="w-full h-full p-[12px] bg-transparent"
-              {...register("price", {
-                required: true,
-                valueAsNumber: true,
-              })}
+              id="title"
+              name="title"
+              placeholder="Enter Course Title"
+              className={`w-full bg-richblack-700 rounded-[0.5rem] text-richblack-5 p-[12px] shadow-richblack`}
+              style={{
+                boxShadow: " 0px -1px 0px 0px rgba(255, 255, 255, 0.18) inset",
+              }}
+              {...register("title", { required: true })}
             />
+            {errors.title && (
+              <span className="text-pink-200 text-sm">
+                Course Title is required
+              </span>
+            )}
           </div>
-          {errors.price && (
-            <span className="text-pink-200 text-sm">
-              Course Price is required
-            </span>
-          )}
-        </div>
-        <div className="grid gap-y-1">
-          <label htmlFor="language">
-            Language <sup className=" text-[0.725rem] text-pink-200">*</sup>
-          </label>
-          <input
-            type="text"
-            id="language"
-            name="language"
-            placeholder="Enter Course Language"
-            className={`w-full bg-richblack-700 rounded-[0.5rem] text-richblack-5 p-[12px] shadow-richblack`}
-            style={{
-              boxShadow: " 0px -1px 0px 0px rgba(255, 255, 255, 0.18) inset",
-            }}
-            {...register("language", { required: true })}
+          <div className="grid gap-y-1">
+            <label htmlFor="description">
+              Course Short Description{" "}
+              <sup className=" text-[0.725rem] text-pink-200">*</sup>
+            </label>
+            <textarea
+              type="text"
+              id="description"
+              name="description"
+              placeholder="Enter Course Short Description"
+              rows={3}
+              className={`w-full bg-richblack-700 rounded-[0.5rem] text-richblack-5 p-[12px] shadow-richblack`}
+              style={{
+                boxShadow: " 0px -1px 0px 0px rgba(255, 255, 255, 0.18) inset",
+              }}
+              {...register("description", { required: true })}
+            ></textarea>
+            {errors.description && (
+              <span className="text-pink-200 text-sm">
+                Course Short Description is required
+              </span>
+            )}
+          </div>
+          <div className="grid gap-y-1">
+            <label htmlFor="price">
+              Course Price{" "}
+              <sup className=" text-[0.725rem] text-pink-200">*</sup>
+            </label>
+            <div
+              style={{
+                boxShadow: " 0px -1px 0px 0px rgba(255, 255, 255, 0.18) inset",
+              }}
+              className="w-full flex items-center relative bg-richblack-700 rounded-[0.5rem] text-richblack-5 shadow-richblack"
+            >
+              <HiOutlineCurrencyRupee
+                className=" bottom-4 ml-3 opacity-70"
+                size={23}
+              />
+              <input
+                type="text"
+                id="price"
+                name="price"
+                placeholder="Enter Course Price"
+                className="w-full h-full p-[12px] bg-transparent"
+                {...register("price", {
+                  required: true,
+                  valueAsNumber: true,
+                })}
+              />
+            </div>
+            {errors.price && (
+              <span className="text-pink-200 text-sm">
+                Course Price is required
+              </span>
+            )}
+          </div>
+          <div className="grid gap-y-1">
+            <label htmlFor="language">
+              Language <sup className=" text-[0.725rem] text-pink-200">*</sup>
+            </label>
+            <input
+              type="text"
+              id="language"
+              name="language"
+              placeholder="Enter Course Language"
+              className={`w-full bg-richblack-700 rounded-[0.5rem] text-richblack-5 p-[12px] shadow-richblack`}
+              style={{
+                boxShadow: " 0px -1px 0px 0px rgba(255, 255, 255, 0.18) inset",
+              }}
+              {...register("language", { required: true })}
+            />
+            {errors.title && (
+              <span className="text-pink-200 text-sm">
+                Course Title is required
+              </span>
+            )}
+          </div>
+          <div className="grid gap-y-1">
+            <label htmlFor="category">
+              Course Category{" "}
+              <sup className=" text-[0.725rem] text-pink-200">*</sup>
+            </label>
+            <select
+              name="category"
+              id="category"
+              defaultValue=""
+              className="w-full h-full bg-richblack-700 rounded-[0.5rem] text-richblack-5 md:p-[12px] py-[12px] shadow-richblack"
+              style={{
+                boxShadow: " 0px -1px 0px 0px rgba(255, 255, 255, 0.18) inset",
+              }}
+              {...register("category", {
+                required: true,
+              })}
+            >
+              <option value="" disabled>
+                Choose a Category
+              </option>
+              {!loading &&
+                courseCategories.map((category) => (
+                  <option key={category._id} value={category._id}>
+                    {category.name}
+                  </option>
+                ))}
+            </select>
+            {errors.category && (
+              <span className="text-pink-200 text-sm">
+                Course Category is required
+              </span>
+            )}
+          </div>
+          <TagsInput setValue={setValue} errors={errors} />
+          <Upload
+            label="Course Thumbnail"
+            register={register}
+            name="file"
+            setValue={setValue}
+            multiple={false}
+            errors={errors}
+            fileState={setThumbnailMeta}
           />
-          {errors.title && (
-            <span className="text-pink-200 text-sm">
-              Course Title is required
-            </span>
-          )}
-        </div>
-        <div className="grid gap-y-1">
-          <label htmlFor="category">
-            Course Category{" "}
-            <sup className=" text-[0.725rem] text-pink-200">*</sup>
-          </label>
-          <select
-            name="category"
-            id="category"
-            defaultValue=""
-            className="w-full h-full bg-richblack-700 rounded-[0.5rem] text-richblack-5 md:p-[12px] py-[12px] shadow-richblack"
-            style={{
-              boxShadow: " 0px -1px 0px 0px rgba(255, 255, 255, 0.18) inset",
-            }}
-            {...register("category", {
-              required: true,
-            })}
-          >
-            <option value="" disabled>
-              Choose a Category
-            </option>
-            {!loading &&
-              courseCategories.map((category) => (
-                <option key={category._id} value={category._id}>
-                  {category.name}
-                </option>
-              ))}
-          </select>
-          {errors.category && (
-            <span className="text-pink-200 text-sm">
-              Course Category is required
-            </span>
-          )}
-        </div>
-        <TagsInput setValue={setValue} errors={errors} />
-        <Upload
-          label="Course Thumbnail"
-          register={register}
-          name="file"
-          setValue={setValue}
-          multiple={false}
-          errors={errors}
-          fileState={setThumbnailMeta}
-        />
-        {/* <div className="grid gap-y-1">
+          {/* <div className="grid gap-y-1">
           <label>
             Benifits of the course{" "}
             <sup className=" text-[0.725rem] text-pink-200">*</sup>
@@ -287,29 +303,30 @@ function CourseInformationForm() {
             </span>
           )}
         </div> */}
-        <KeyFeatures
-          name="keyFeatures"
-          label="Key Features"
-          register={register}
-          errors={errors}
-          setValue={setValue}
-          getValues={getValues}
-        />
-        <div className="w-full flex gap-4 items-center justify-end">
-          {editCourse && (
-            <Button
-              type="button"
-              className="!bg-richblack-700 w-1/2"
-              onClick={() => dispatch(setStep(2))}
-            >
-              Continue without saving
+          <KeyFeatures
+            name="keyFeatures"
+            label="Key Features"
+            register={register}
+            errors={errors}
+            setValue={setValue}
+            getValues={getValues}
+          />
+          <div className="w-full flex gap-4 items-center justify-end">
+            {editCourse && (
+              <Button
+                type="button"
+                className="!bg-richblack-700 w-1/2"
+                onClick={() => dispatch(setStep(2))}
+              >
+                Continue without saving
+              </Button>
+            )}
+            <Button type="submit" active={true} className="w-1/3">
+              {editCourse ? "Save Changes" : "Next"}
             </Button>
-          )}
-          <Button type="submit" active={true} className="w-1/3">
-            {editCourse ? "Save Changes" : "Next"}
-          </Button>
-        </div>
-      </form>
+          </div>
+        </form>
+      )}
     </div>
   );
 }
--- src/services/aws/s3Services.js
+++ src/services/aws/s3Services.js
@@ -5,17 +5,19 @@ import toast from "react-hot-toast";
 
 const { GET_S3_UPLOAD_URL } = awsServicesEndpoints;
 
-export async function uploadToS3(fileMeta, file) {
+export async function uploadToS3(fileMeta, file, assetType) {
   try {
-    const res = await apiConnector("POST", GET_S3_UPLOAD_URL, fileMeta);
+    const res = await apiConnector("POST", GET_S3_UPLOAD_URL, {
+      fileMeta,
+      assetType,
+    });
     if (res.success) {
       const uploadResponse = await axios.put(res.data.url, file, {
         headers: {
           "Content-Type": file.type,
         },
       });
-      console.log(uploadResponse);
-      return { uploadResponse, fileKey: res.data.thumbnailInfo };
+      return { uploadResponse, fileKey: res.data.fileInfo };
     } else {
       return toast.error(res.message);
     }
